2965b3a58f6b63b388faf88fc95e1e4f83eed6f6
colin <colin@groove.(none)>
2009-07-14 16:25:37 -0600

    New run script...

--- run.sh
+++ run.sh
@@ -1,20 +1,7 @@
 #!/bin/sh
 
-OPTS="-m 2500 -n 2000 -g 10000 -l -s 6 -r 10"
+OPTS="-m 6500 -n 10000 -g 10000 --peaks --graininit --voronei --savemem"
 
-#generate a graininit
-./crystal -d ./ -n 2 -g 10000 -q --graininit --voronei
-
-for diff in 0 0.9 0.95 0.996
-do
-	for angle in 0 1 2 20 200
-	do
-		mkdir -p data-$diff-$angle
-		cp graininit data-$diff-$angle/
-		./crystal $OPTS -d data-$diff-$angle -D $diff -a $angle
-#		ffmpeg -i data-$diff-$angle/slope.%05d.png -b 20000k slope-$diff-$angle.mpg
-#		ffmpeg -i data-$diff-$angle/level.%05d.png -b 20000k level-$diff-$angle.mpg
-	done
-done
-
+./crystal $OPTS -d data14 -s 14
+./crystal $OPTS -d data6  -s 6
 

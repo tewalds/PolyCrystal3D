d94d16542c1bf25c40e51061c07539e205611cca
colin <colin@groove.(none)>
2009-07-14 16:26:32 -0600

    Bah, stupid typos

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 OPTS="-m 6500 -n 10000 -g 10000 --peaks --graininit --voronei --savemem"
 
-./crystal $OPTS -d data14 -s 14
-./crystal $OPTS -d data6  -s 6
+./crystal $OPTS -d long14 -s 14
+./crystal $OPTS -d long6  -s 6
 
